f95ce5cd3878ed19fd6a0e1f5c1b6524cdf2a8f0
banujan1995 <50479506+banujan1995@users.noreply.github.com>
2021-05-15 16:52:05 +0200

    correction db

--- src/main/java/ch/zt/timerecorders/start/MitarbeiterRepositoryInterface.java
+++ src/main/java/ch/zt/timerecorders/start/MitarbeiterRepositoryInterface.java
@@ -1,21 +1,23 @@
-package ch.zt.timerecorders.start;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-
-/**
- * @author Kiren Gondal
- */
-@Repository
-public interface MitarbeiterRepositoryInterface extends JpaRepository<MitarbeiterRegister, Long> {
-	
-	public List<MitarbeiterRegister> findByMitarbeiterID(long mitarbeiterID);
-	
-
-	 
-
-}
+//package ch.zt.timerecorders.start;
+//
+//import java.util.List;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import ch.zt.timerecorders.database.MitarbeiterRegister;
+//
+//
+///**
+// * @author Kiren Gondal
+// */
+////@Repository
+////public interface MitarbeiterRepositoryInterface extends JpaRepository<MitarbeiterRegister, Long> {
+////	
+////	public List<MitarbeiterRegister> findByMitarbeiterID(long mitarbeiterID);
+////	
+////
+////	 
+////
+////}
