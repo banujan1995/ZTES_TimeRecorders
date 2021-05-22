64835c6ce66baa972136420bb8bf2ca2fd90905a
banujan1995 <50479506+banujan1995@users.noreply.github.com>
2021-05-22 23:24:53 +0200

    referenz to path

--- src/main/java/ch/zt/timerecorders/services/UserLoginActivityService.java
+++ src/main/java/ch/zt/timerecorders/services/UserLoginActivityService.java
@@ -30,16 +30,17 @@ import ch.zt.timerecorders.start.TimeStampRegisterChangeInterface;
  *
  */
 
- /*
-  * Hier soll die Anmeldeaktivitäten von den User notiert werden. Es wird eine Liste geführt 
-  * mit den Anmeldungen und bei einer Abmeldung wird der User aus der Liste rausgenommen. 
-  */
+/*
+ * Hier soll die Anmeldeaktivitäten von den User notiert werden. Es wird eine
+ * Liste geführt mit den Anmeldungen und bei einer Abmeldung wird der User aus
+ * der Liste rausgenommen.
+ */
 
 @RestController
 public class UserLoginActivityService {
-	
+
 	Logger logger = ServiceLocator.getServiceLocator().getLogger();
-	
+
 	@Autowired
 	private MitarbeiterRepository mitarbeiterRepository;
 
@@ -54,56 +55,67 @@ public class UserLoginActivityService {
 
 	@Autowired
 	private TimeStampRegisterChangeInterface timeStampRegisterChange;
-	
+
 	@Autowired
 	private LoginUserActivityRegisterInterface loginUserActivityRegisterInterface;
-	
 
-	
-	
 	/*
 	 * Wir holen wir den User anhand der IPAdresse. (BR)
 	 */
 	@ResponseBody
 	@GetMapping(path = "/timerecorders/getUserOnlineList/", produces = "application/json")
 	public List<LoginUserActivityRegister> getUserByIP() { // Filter Framework
-		
+
 		List<LoginUserActivityRegister> userOnline = loginUserActivityRegisterInterface.findAll();
 		logger.info(userOnline.toString() + "User Online wurden übergeben");
-		
+
 		return userOnline;
 	}
-	
-	
-	
+
 	/*
 	 * Neue IP Adresse mit den dazugehörigen User registrieren. (BR)
 	 */
-	
+
 	@PostMapping(path = "/timerecorders/registeruser/", produces = "application/json")
 	public boolean registerUser(@RequestBody MessageLoginUserActivity registeruser) {
 		logger.info("Mitarbeiter ist eingeloggt - wurde in der AktivitätsOnlineListe aufgenommen");
-		
+
 		LoginUserActivityRegister newUser = new LoginUserActivityRegister();
-		
+
 		newUser.setIP(registeruser.getIP());
 		newUser.setUsername(registeruser.getUsername());
-		
-		
+
 		newUser = loginUserActivityRegisterInterface.save(newUser);
 		logger.info("Anmeldung von neuen User wurde erfasst");
-		return true; 
+		return true;
 	}
-	
-	//User loggt sich aus und wird von der Liste entfernt(BR)
-	
+
+	// User loggt sich aus und wird von der Liste entfernt(BR)
+
 	@PostMapping(path = "/timerecorders/deleteuser/", produces = "application/json")
 	public boolean deleteUser(@RequestBody MessageLoginUserActivity deleteuser) {
 		logger.info("Mitarbeiter wurde ausgeloggt - Mitarbeiter wurde aus der Liste gelöscht.");
 		
 		LoginUserActivityRegister localuser; 
+		boolean userDeleted; 
 
 		List<LoginUserActivityRegister> userOnline = loginUserActivityRegisterInterface.findAll();
+		
+		
+		for(int i = 0; i < userOnline.size(); i++) {
+			localuser = userOnline.get(i); 
+			
+			if(userOnline.get(i).getIP().equalsIgnoreCase(deleteuser.getIP()) && userOnline.get(i).getUsername().equalsIgnoreCase(deleteuser.getUsername())) {
+				userOnline.remove(i);
+				userDeleted = true;
+			} else {
+				userDeleted = false;
+			}
+			
+			userOnline = loginUserActivityRegisterInterface.saveAll(userOnline);
+				
+			
+		}
 		
 //		for (int x = 0; x < userOnline; x++ ) {
 //			localuser = user; 
